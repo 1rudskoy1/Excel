29cf0ae795d8c0d2de5c17735af90d948c7b189f
Ganiev Rustam <ganiev.rystam@gmail.com>
2022-07-21 04:04:48 +0300

    resize table finish

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
+import {$} from '@core/dom.js'
 
 export class Table extends ExcelComponent{
 
@@ -21,7 +22,48 @@ export class Table extends ExcelComponent{
     }
     onMousedown(event){
         if(event.target.dataset.resize){
-            console.log("Start");
+            const $resizer = $(event.target);
+            const $parent = $resizer.closest('[data-type = "resizable"]');
+            const coords = $parent.getCoords();
+            const type = event.target.dataset.resize;
+            const sideProp = type === 'col' ? "bottom" : "right"
+            let value;
+
+            $resizer.css({
+                opacity: 1,
+                [sideProp]: "-3000px",
+
+            });
+            document.onmousemove = e =>{
+                if(type === "col"){
+                    const delta = e.pageX - coords.right;
+                    value = coords.width + delta;
+                    $resizer.css({right: -delta + "px"});
+                }else{
+                    const delta = e.pageY - coords.bottom;
+                    value = coords.height + delta;
+                    $resizer.css({bottom: -delta + "px"});
+                }
+            }
+
+            document.onmouseup = () => {
+                document.onmousemove = null;
+                document.onmouseup = null;
+                if(type === "col"){
+                    $parent.css({width : value + "px"});
+                    this.$root.findAll(`[data-col = "${$parent.data.col}"]`).forEach(el => {
+                        el.style.width = value + "px";
+                    });
+                }else{
+                    $parent.css({height : value + "px"});
+                }
+                $resizer.css({
+                    opacity: 0,
+                    bottom: 0,
+                    right: 0
+                });
+
+            }
         }
 
     }
@@ -32,3 +74,13 @@ export class Table extends ExcelComponent{
 
     }
 }
+
+// 113 мс  Сценарии
+// 345 мс  Отрисовка
+// 126 мс  Отображение
+// 375 мс  Система
+
+//60 мс  Сценарии
+// 251 мс  Отрисовка
+// 87 мс  Отображение
+// 233 мс  Система
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,15 +4,15 @@ const CODES = {
     Z: 90,
 };
 
-function toCell(){
+function toCell(_, col){
     return `
-        <div class = "cell" contenditable></div>
+        <div class = "cell" contenditable data-col = "${col}"></div>
     `
 }
 
-function createCol(col){
+function createCol(col, index){
     return  `
-        <div class="column">
+        <div class="column" data-type = "resizable" data-col = "${index}">
             ${col}
             <div class = "col-resize" data-resize = "col"></div> 
         </div>
@@ -22,7 +22,7 @@ function createCol(col){
 function createRow(index, content){
     const resizer = index ?  '<div class = "row-resize" data-resize = "row"></div> ' : '';
     return `
-        <div class = "row"> 
+        <div class = "row" data-type = "resizable"> 
             <div class = "row-info">${index ? index: ''}
             ${resizer}
             </div>
